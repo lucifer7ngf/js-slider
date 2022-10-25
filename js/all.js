b15a265a8a1ea4a231cb89f182fc887a83a26d12
lucifer7ngf <lucifer.ngf@gmail.com>
2022-10-25 14:44:54 +0330

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,9 +1,9 @@
 const progress = document.querySelector('.progress-bar');
 
-window.addEventListener('scroll', ()=> {
+window.addEventListener('scroll', ()=>{
      const winScroll = window.pageYOffset;
     const height = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = (winScroll/height) * 100;
-    progress.style.width = `$[scrolled]%`
+    progress.style.width = `${scrolled}%`
 
 })
